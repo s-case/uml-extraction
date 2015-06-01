94de5e509b14f38c3a3f6f3c75425aeed07cd5c3
Marin Orlic <marin.orlic@ericsson.com>
2015-06-01 22:59:25 +0200

    Add exception for missing JNI libs

---
+++ eu.scasefp7.eclipse.umlrec/src/eu/scasefp7/eclipse/umlrec/MissingRecognizerNativeException.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package eu.scasefp7.eclipse.umlrec;
+
+/**
+ * @author emaorli
+ *
+ */
+public class MissingRecognizerNativeException extends Exception {
+
+	/**
+	 * Serialization UID
+	 */
+	private static final long serialVersionUID = -7139299530869543310L;
+	
+	
+	public MissingRecognizerNativeException() {
+		super();
+	}
+
+	
+	public MissingRecognizerNativeException(String message) {
+		super(message);
+	}
+
+}
