831265750ff7f97d8c636ae67da5726192603938
thdiaman <themisdiamantopoulos@hotmail.com>
2016-04-13 14:18:20 +0300

    Fix to avoid errors when UML diagrams are not annotated

--- eu.scasefp7.eclipse.umlrec/src/eu/scasefp7/eclipse/umlrec/ui/parser/WriteStaticOntology.java
+++ eu.scasefp7.eclipse.umlrec/src/eu/scasefp7/eclipse/umlrec/ui/parser/WriteStaticOntology.java
@@ -21,84 +21,93 @@ public class WriteStaticOntology {
 				ontology.connectRequirementToConcept(diagramName, node.getName());
 
 			} else if (node.getType().equals("uml:UseCaseNode")) {
-				String[] actionAndObject = WriteDynamicOntology.getActionAndObject(node.getName(),
-						node.getAnnotations());
-				String action = actionAndObject[0];
-				String object1 = actionAndObject[1];
-				ontology.addAction(action);
-				ontology.connectRequirementToOperation(diagramName, action);
-				ontology.addObject(object1);
-				ontology.connectRequirementToConcept(diagramName, object1);
-				ontology.connectActionToObject(action, object1);
-
+				if (!node.getAnnotations().equals("")) {
+					String[] actionAndObject = WriteDynamicOntology.getActionAndObject(node.getName(),
+							node.getAnnotations());
+					String action = actionAndObject[0];
+					String object1 = actionAndObject[1];
+					ontology.addAction(action);
+					ontology.connectRequirementToOperation(diagramName, action);
+					ontology.addObject(object1);
+					ontology.connectRequirementToConcept(diagramName, object1);
+					ontology.connectActionToObject(action, object1);
+				}
 			}
 
 		}
 
 		for (XMIUseCaseNode node : nodes) {
 			if (node.getType().equals("uml:UseCaseNode")) {
-				String[] actionAndObject = WriteDynamicOntology.getActionAndObject(node.getName(),
-						node.getAnnotations());
-				String action = actionAndObject[0];
-				String object1 = actionAndObject[1];
+				if (!node.getAnnotations().equals("")) {
 
-				// has actor or connected use case
-				boolean nodeIsConnectedToActor = false;
+					String[] actionAndObject = WriteDynamicOntology.getActionAndObject(node.getName(),
+							node.getAnnotations());
+					String action = actionAndObject[0];
+					String object1 = actionAndObject[1];
 
-				for (String id : node.getincomingSolidNode()) {
-					for (XMIUseCaseNode connectedNode : nodes) {
-						if (connectedNode.getId().equals(id) && connectedNode.getType().equals("uml:UserNode")) {
-							ontology.connectActorToAction(connectedNode.getName(), action);
-							nodeIsConnectedToActor = true;
-						} else if (connectedNode.getId().equals(id)
-								&& connectedNode.getType().equals("uml:UseCaseNode")) {
-							String[] actionAndObject2 = WriteDynamicOntology.getActionAndObject(
-									connectedNode.getName(), connectedNode.getAnnotations());
-							String object2 = actionAndObject2[1];
-							ontology.connectObjectToObject(object1, object2);
-						}
-					}
-				}
+					// has actor or connected use case
+					boolean nodeIsConnectedToActor = false;
 
-				for (String id : node.getconnectedSolidNode()) {
-					for (XMIUseCaseNode connectedNode : nodes) {
-						if (connectedNode.getId().equals(id) && connectedNode.getType().equals("uml:UserNode")) {
-							ontology.connectActorToAction(connectedNode.getName(), action);
-							nodeIsConnectedToActor = true;
-						}
-					}
-				}
-				for (String id : node.getconnectedSolidNode()) {
-					for (XMIUseCaseNode connectedNode : nodes) {
-						if (connectedNode.getId().equals(id) && connectedNode.getType().equals("uml:UseCaseNode")) {
-							if (nodeIsConnectedToActor) {
-								String[] actionAndObject2 = WriteDynamicOntology.getActionAndObject(
-										connectedNode.getName(), connectedNode.getAnnotations());
-								String object2 = actionAndObject2[1];
-								ontology.connectObjectToObject(object1, object2);
-							}
-						}
-					}
-				}
-
-				// extends or includes
-				for (String id : node.getincomingDashedNode()) {
-					for (XMIUseCaseNode connectedNode : nodes) {
-						if (connectedNode.getId().equals(id) && connectedNode.getType().equals("uml:UseCaseNode")) {
-							for (XMIEdge edge : edges) {
-								if (((XMIUseCaseNode) edge.getSourceNode()).getId().equals(id)
-										&& ((XMIUseCaseNode) edge.getTargetNode()).getId().equals(node.getId())) {
+					for (String id : node.getincomingSolidNode()) {
+						for (XMIUseCaseNode connectedNode : nodes) {
+							if (connectedNode.getId().equals(id) && connectedNode.getType().equals("uml:UserNode")) {
+								ontology.connectActorToAction(connectedNode.getName(), action);
+								nodeIsConnectedToActor = true;
+							} else if (connectedNode.getId().equals(id)
+									&& connectedNode.getType().equals("uml:UseCaseNode")) {
+								if (!connectedNode.getAnnotations().equals("")) {
 									String[] actionAndObject2 = WriteDynamicOntology.getActionAndObject(
 											connectedNode.getName(), connectedNode.getAnnotations());
 									String object2 = actionAndObject2[1];
-									if (edge.getName().equalsIgnoreCase("extend")) {
+									ontology.connectObjectToObject(object1, object2);
+								}
+							}
+						}
+					}
+
+					for (String id : node.getconnectedSolidNode()) {
+						for (XMIUseCaseNode connectedNode : nodes) {
+							if (connectedNode.getId().equals(id) && connectedNode.getType().equals("uml:UserNode")) {
+								ontology.connectActorToAction(connectedNode.getName(), action);
+								nodeIsConnectedToActor = true;
+							}
+						}
+					}
+					for (String id : node.getconnectedSolidNode()) {
+						for (XMIUseCaseNode connectedNode : nodes) {
+							if (connectedNode.getId().equals(id) && connectedNode.getType().equals("uml:UseCaseNode")) {
+								if (nodeIsConnectedToActor) {
+									if (!connectedNode.getAnnotations().equals("")) {
+										String[] actionAndObject2 = WriteDynamicOntology.getActionAndObject(
+												connectedNode.getName(), connectedNode.getAnnotations());
+										String object2 = actionAndObject2[1];
 										ontology.connectObjectToObject(object1, object2);
-									} else if (edge.getName().equalsIgnoreCase("include")) {
-										ontology.connectObjectToObject(object2, object1);
 									}
 								}
 							}
+						}
+					}
 
+					// extends or includes
+					for (String id : node.getincomingDashedNode()) {
+						for (XMIUseCaseNode connectedNode : nodes) {
+							if (connectedNode.getId().equals(id) && connectedNode.getType().equals("uml:UseCaseNode")) {
+								for (XMIEdge edge : edges) {
+									if (((XMIUseCaseNode) edge.getSourceNode()).getId().equals(id)
+											&& ((XMIUseCaseNode) edge.getTargetNode()).getId().equals(node.getId())) {
+										if (!connectedNode.getAnnotations().equals("")) {
+											String[] actionAndObject2 = WriteDynamicOntology.getActionAndObject(
+													connectedNode.getName(), connectedNode.getAnnotations());
+											String object2 = actionAndObject2[1];
+											if (edge.getName().equalsIgnoreCase("extend")) {
+												ontology.connectObjectToObject(object1, object2);
+											} else if (edge.getName().equalsIgnoreCase("include")) {
+												ontology.connectObjectToObject(object2, object1);
+											}
+										}
+									}
+								}
+							}
 						}
 					}
 				}
